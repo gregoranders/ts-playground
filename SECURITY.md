4dccd5525e8e55ec183c6cc8654fc13b43448715
Gregor Anders <gregor.anders@gmail.com>
2020-06-15 16:14:20 +0200

    refactor(other): refactor
    - refactor jest testing to use common path for results
    - include jest-html-reporter jest-spec-reporter jest-junit
    - move prettier and eslint into package.json
    - add api-extractor/api-documenter
    - refactor package.json
    
    #34

---
+++ SECURITY.md
@@ -0,0 +1,22 @@
+# Security Policy
+
+## Supported Versions
+
+​
+| Version | Supported |
+| ------- | ------------------ |
+| 0.0.10 | :white_check_mark: |
+| 0.0.9 | :x: |
+| 0.0.8 | :x: |
+| 0.0.7 | :x: |
+| 0.0.6 | :x: |
+| 0.0.5 | :x: |
+| 0.0.4 | :x: |
+| 0.0.3 | :x: |
+| 0.0.2 | :x: |
+| 0.0.1 | :x: |
+​
+
+## Reporting a Vulnerability
+
+[Create Issue](https://github.com/gregoranders/ts-playground/issues/new?labels=bug&template=bug_report.md&title=Security+Issue)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
-const strip: RegExp[] = [/^refs\/tags\//, /^refs\/heads\//, /^\//, /\/$/];
+/**
+ * ts-playground
+ *
+ * @packageDocumentation
+ */
 
-export const normalizeBranchName = (branch: string): string => {
-  let clean = branch;
-  strip.forEach((regex) => {
-    clean = clean.replace(regex, '');
-  });
-  return clean;
-};
+ /**
+  * @internal
+  */
+ const strip: RegExp[] = [/^refs\/tags\//, /^refs\/heads\//, /^\//, /\/$/];
+
+ /**
+  * normalize `branch name`
+  *
+  * @example
+  * ```ts
+  * const branch = normalize(`refs/heads/master`);
+  *
+  * expect(branch).toEqual('master');
+  * ```
+  *
+  * @param branch - `branch name`
+  * @returns normalized `branch name`
+  *
+  * @public
+  */
+ export function normalizeBranchName(branch: string): string {
+   let clean = branch;
+   strip.forEach((regex) => {
+     clean = clean.replace(regex, '');
+   });
+   return clean;
+ }
+
+ /**
+  * default export
+  *
+  * @public
+  */
+ export default normalizeBranchName;
