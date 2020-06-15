4be2fbc0ca51faa7be25c9d801f1eeff2b7f6b55
Gregor Anders <gregor.anders@gmail.com>
2020-06-15 16:57:33 +0200

    fix(other): fix format
    fix format
    
    #34 - refactor(other): refactor

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,37 @@
  * @packageDocumentation
  */
 
- /**
-  * @internal
-  */
- const strip: RegExp[] = [/^refs\/tags\//, /^refs\/heads\//, /^\//, /\/$/];
+/**
+ * @internal
+ */
+const strip: RegExp[] = [/^refs\/tags\//, /^refs\/heads\//, /^\//, /\/$/];
 
- /**
-  * normalize `branch name`
-  *
-  * @example
-  * ```ts
-  * const branch = normalize(`refs/heads/master`);
-  *
-  * expect(branch).toEqual('master');
-  * ```
-  *
-  * @param branch - `branch name`
-  * @returns normalized `branch name`
-  *
-  * @public
-  */
- export function normalizeBranchName(branch: string): string {
-   let clean = branch;
-   strip.forEach((regex) => {
-     clean = clean.replace(regex, '');
-   });
-   return clean;
- }
+/**
+ * normalize `branch name`
+ *
+ * @example
+ * ```ts
+ * const branch = normalize(`refs/heads/master`);
+ *
+ * expect(branch).toEqual('master');
+ * ```
+ *
+ * @param branch - `branch name`
+ * @returns normalized `branch name`
+ *
+ * @public
+ */
+export function normalizeBranchName(branch: string): string {
+  let clean = branch;
+  strip.forEach((regex) => {
+    clean = clean.replace(regex, '');
+  });
+  return clean;
+}
 
- /**
-  * default export
-  *
-  * @public
-  */
- export default normalizeBranchName;
+/**
+ * default export
+ *
+ * @public
+ */
+export default normalizeBranchName;
